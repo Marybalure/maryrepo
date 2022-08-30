15da1a2099ce04ba1511e18089f22f3ef32a34de
MaryLilly <111157786+Marybalure@users.noreply.github.com>
2022-08-31 00:24:59 +0530

    Update aws ec2 run-instances.sh

--- aws ec2 run-instances.sh
+++ aws ec2 run-instances.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-aws_instance=$"(aws ec2 run-instances \ 
-    --image-id ami-06489866022e12a14 \ 
-    --count 1 \ 
-    --instance-type t2.micro \
-    --key-name Mary_18-ubuntu_keypair \ 
-    --subnet-id subnet-0e8281328042b2fa4 \ 
-    --security-group-ids sg-06bb607ae83ff084f \
-    --user-data file://myuserdatascript1.sh)"
-    
- echo "$(aws_instance)"
+aws_instance="$(aws ec2 run-instances --image-id ami-06489866022e12a14 --count 1 --instance-type t2.micro --key-name Mary_18-ubuntu_keypair 
+--subnet-id subnet-0e8281328042b2fa4 --security-group-ids sg-06bb607ae83ff084f --user-data file://myuserdatascript1.sh)"
+echo "created an ec2 instance and passed userdata as bootstrap script."
