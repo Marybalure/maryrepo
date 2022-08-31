447453a164041de53956d28cf176890e40f39e75
MaryLilly <111157786+Marybalure@users.noreply.github.com>
2022-08-31 14:46:29 +0530

    Update aws ec2 run-instances.sh

--- aws ec2 run-instances.sh
+++ aws ec2 run-instances.sh
@@ -4,5 +4,5 @@
 aws_instance="$(aws ec2 run-instances --image-id ami-06489866022e12a14 --count 1 --instance-type t2.micro --key-name Mary_18-ubuntu_keypair 
 --subnet-id subnet-0e8281328042b2fa4 --security-group-ids sg-06bb607ae83ff084f --user-data file://myuserdatascript1.sh)"
 
-echo $aws_instance
 echo "created an ec2 instance and passed userdata as bootstrap script."
+# verify in EC2 management console if ec2 instance is created and connect to ec2 machine and verify it apcge server is installed on this machine.
